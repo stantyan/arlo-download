fe40a539fe109eb1a703ca800fcc2bd8c08b45c6
Stan Tyan <stantyan@yahoo.com>
2018-04-23 16:39:14 +0300

    update-path

--- arlo-cron.sh
+++ arlo-cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PATH=/home/arlo/anaconda3/bin
-python /home/arlo/arlo/arlo.py
+python /home/arlo/arlo/arlo-download.py
 rclone move -v --transfers 12 --ignore-existing /home/arlo/arlo/library/ gd:/apps/arlo.netgear.com/library/2018/
 echo "Arlo daily backup completed"
