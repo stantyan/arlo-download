c8e38f1a5fe5e215cddebf0dad7eedec981b0892
Stan Tyan <stantyan@yahoo.com>
2018-04-23 16:38:37 +0300

    update-rclone

--- rclone.sh
+++ rclone.sh
@@ -1,3 +1,3 @@
-alias arlo_backup_dry = "rclone move -v --transfers 12 --ignore-existing --exclude '.*' --dry-run ~/arlo/library/ gd:/apps/arlo.netgear.com/library/2018/"
+alias arlo_backup_dry = 'rclone move -v --transfers 12 --ignore-existing --dry-run /home/arlo/arlo/library/ gd:/apps/arlo.netgear.com/library/2018/'
 
-alias arlo_backup = "rclone move -v --transfers 12 --ignore-existing --exclude '.*' ~/arlo/library/ gd:/apps/arlo.netgear.com/library/2018/"
+alias arlo_backup = 'rclone move -v --transfers 12 --ignore-existing /home/arlo/arlo/library/ gd:/apps/arlo.netgear.com/library/2018/'
