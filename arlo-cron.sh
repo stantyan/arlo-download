f0083c044bc6587f51afcdaeff56856a3b115acf
Stan Tyan <stantyan@yahoo.com>
2018-04-24 10:28:26 +0300

    add-email-notification

--- arlo-cron.sh
+++ arlo-cron.sh
@@ -2,4 +2,5 @@
 PATH=/home/arlo/anaconda3/bin
 python /home/arlo/arlo/arlo-download.py
 rclone move -v --transfers 12 --ignore-existing /home/arlo/arlo/library/ gd:/apps/arlo.netgear.com/library/2018/
+mail -s "Arlo backup for yesterday is complete" user@example.com < /dev/null
 echo "Arlo daily backup completed"
